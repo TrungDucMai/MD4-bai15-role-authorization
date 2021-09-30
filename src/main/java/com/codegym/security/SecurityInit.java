d78de2d692dc0023bff40b7ca2820169f9e00eea
TrungDucMai <trungmaiduc196@gmail.com>
2021-09-30 22:18:22 +0700

    '1'

---
+++ src/main/java/com/codegym/security/SecurityInit.java
@@ -0,0 +1,8 @@
+package com.codegym.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+
+}
